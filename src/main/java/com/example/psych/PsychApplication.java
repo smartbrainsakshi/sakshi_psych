70b0109a94631fbe6cf6e1f6821eb88ad57c6663
sakshi <sakshipooja21@gmail.com>
2020-02-21 03:01:36 +0530

    first commit

---
+++ src/main/java/com/example/psych/PsychApplication.java
@@ -0,0 +1,13 @@
+package com.example.psych;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsychApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/psych/PsychApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.psych;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsychApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
